8402579798235d67fa7a84da248749ea75b956c1
seunghee <victoria9981@naver.com>
2026-03-21 15:52:19 +0900

    feat: 드롭다운 ui수정

--- imagepicker/src/main/java/com/universe/imagepicker/presentation/gallery/AlbumDropdown.kt
+++ imagepicker/src/main/java/com/universe/imagepicker/presentation/gallery/AlbumDropdown.kt
@@ -1,12 +1,19 @@
 package com.universe.imagepicker.presentation.gallery
 
+import android.provider.CalendarContract
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.universe.imagepicker.domain.model.GalleryAlbum
 
 /**
@@ -23,8 +30,24 @@ fun AlbumDropdown(
     onAlbumSelected: (GalleryAlbum) -> Unit,
 ) {
     Box(modifier = modifier) {
-        TextButton(onClick = openDropDown) {
-            Text(text = selectedAlbum?.name ?: "전체")
+        Button(
+            onClick = openDropDown,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Transparent,
+            ),
+        ) {
+            Row {
+                Text(
+                    text = selectedAlbum?.name ?: "전체",
+                    color = Color.Black
+                )
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "드롭다운",
+                    tint = Color.Black
+                )
+            }
         }
         DropdownMenu(
             expanded = dropDownExpanded,
