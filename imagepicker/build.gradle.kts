85d21e6714b7ea5a8ca7bc4f0974f86465726ad4
송승희 <victoria9981@fasoo.com>
2026-03-25 11:33:32 +0900

    feat: 불필요 종속성 제거

--- imagepicker/build.gradle.kts
+++ imagepicker/build.gradle.kts
@@ -118,6 +118,4 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
